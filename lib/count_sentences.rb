19e1cc6a75e37466f3cdb17dc5e60f8c5382375e
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-30 18:09:06 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     count = 0 
-    count = self.split(/[?!.]\s+[A-Z]/).length
+    count = self.scan(/[^\.!?]+[\.!?]/).map(&:strip).length
     count 
   end
   
